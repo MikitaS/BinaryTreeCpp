521fcbedd95b1597e150e931267578fd15b847d3
Mikita <nikitastrizhak@mail.ru>
2019-02-23 20:55:35 +0300

    add tree library

---
+++ tree.cpp
@@ -0,0 +1,127 @@
+#include <string>
+#include <iostream>
+#include "tree.hpp"
+
+///Constructors and destructor:
+
+tree::tree() : head_(new TreeElement), current_(head_)
+{
+	head_->left_ = 0;
+	head_->right_ = 0;
+	head_->parent_ = 0;
+	head_->data_ = "";
+}
+
+tree::tree(std::string str) : head_(new TreeElement), current_(head_)
+{
+	head_->left_ = 0;
+	head_->right_ = 0;
+	head_->parent_ = 0;
+	head_->data_ = str;
+}
+
+tree::~tree()
+{
+	delete_tree(head_);
+}
+
+///Methods:
+
+void tree::delete_tree(TreeElement * first)
+{
+	if(first -> left_)
+		delete_tree(first -> left_);
+	if(first -> right_)
+		delete_tree(first -> right_);
+		
+	first -> left_ = 0;
+	first -> right_ = 0;
+	
+	delete first;
+}
+
+bool tree::go_left()
+{
+	if(!current_->left_)
+		return 0;
+	current_ = current_->left_;
+	return 1;
+}
+
+bool tree::go_right()
+{
+	if(!current_->right_)
+		return 0;
+	current_ = current_->right_;
+	return 1;
+}
+
+bool tree::go_back()
+{
+	if(!current_->parent_)
+		return 0;
+	current_ = current_->parent_;
+	return 1;
+}
+
+void tree::go_head()
+{
+	current_ = head_;
+}
+
+bool tree::add_left(std::string str)
+{
+	if(current_ -> left_)
+		return 0;
+		
+	current_ -> left_ = new TreeElement;
+	current_ -> left_ -> data_ = str;
+	current_ -> left_ -> left_ = 0;
+	current_ -> left_ -> right_ = 0;
+	current_ -> left_ -> parent_ = current_;
+	
+	return 1;
+}
+
+bool tree::add_right(std::string str)
+{
+	if(current_ -> right_)
+		return 0;
+		
+	current_ -> right_ = new TreeElement;
+	current_ -> right_ -> data_ = str;
+	current_ -> right_ -> left_ = 0;
+	current_ -> right_ -> right_ = 0;
+	current_ -> right_ -> parent_ = current_;
+	
+	return 1;
+}
+
+bool tree::del_left()
+{
+	if(!current_ -> left_)
+		return 0;
+		
+	delete_tree(current_ -> left_);
+	current_ -> left_ = 0;
+	
+	return 1;
+}
+
+bool tree::del_right()
+{
+	if(!current_ -> right_)
+		return 0;
+		
+	delete_tree(current_ -> right_);
+	current_ -> right_ = 0;
+	
+	return 1;
+}
+
+std::string & tree::data()
+{
+	return current_->data_;
+}
+
+
---
+++ tree.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <string>
+
+struct TreeElement
+{
+	std::string data_;
+	TreeElement * parent_;
+	TreeElement * left_;
+	TreeElement * right_;
+	
+};
+
+class tree
+{
+public:
+///Constructors and destructor:
+	tree(); //done
+	explicit tree(std::string str);//done
+	~tree();//done
+
+///Methods:
+	/*move current pointer
+	 * return 0 if your way is empty*/
+	bool go_left();//done
+	bool go_right();//done
+	bool go_back();//done
+	void go_head();//done
+	
+	/*add new element
+	 * return 0 if you way is already exist*/
+	bool add_left(std::string data);//done
+	bool add_right(std::string data);//done
+	
+	/*delete element
+	 * return 0 if your way is empty*/
+	bool del_right();//done
+	bool del_left();//done
+	
+	/*return link to a data of current element*/
+	std::string & data(); //done
+
+private:
+	/*delete tree starting from pointer*/
+	void delete_tree(TreeElement * first); //done
+
+	TreeElement * head_;
+	TreeElement * current_;
+	
+};
